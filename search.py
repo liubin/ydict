40370526f4ead3866cce07bd4ff198a47211f55f
bin liu <liubin0329@gmail.com>
2019-07-07 21:11:07 +0800

    add local cache

--- search.py
+++ search.py
@@ -4,20 +4,7 @@ from lxml import etree
 import sys
 import os
 
-reload(sys)
-sys.setdefaultencoding( "utf-8" )
-
-if len(sys.argv) != 2:
-	print "please use python search.py <word>"
-	exit(1)
-
-word = sys.argv[1]
-
-url = "http://dict.youdao.com/w/eng/%s/#keyfrom=dict2.index" % word
-
-html = requests.get(url)
-
-doc = etree.HTML(html.text)
+data_dir = ".dict"
 
 def pattern_1(doc):
 	attr = doc.xpath('//*[@id="phrsListTab"]/div[2]')
@@ -51,26 +38,71 @@ def pattern_2(doc):
 def download_audio(word):
 	url = "http://dict.youdao.com/dictvoice?audio=%s&type=1" % word
 	html = requests.get(url)
-	file_name = "/tmp/%s-audio.mp3" % word
+	file_dir = "%s/%s/" % (data_dir, word)
+	os.system("mkdir -p %s" % file_dir)
+
+	file_name = "%s/audio.mp3" % file_dir
 	file = open(file_name, 'wb')
 	file.write(html.content)
 	file.close()
 	return file_name
 
-s = pattern_1(doc)
+def save_content(word, content):
+	file_dir = "%s/%s/" % (data_dir, word)
+	os.system("mkdir -p %s" % file_dir)
+	file_name = "%s/content.txt" % file_dir
+	file = open(file_name, 'wb')
+	file.write(content)
+	file.close()
+
+def load_from_net(word):
+
+	url = "http://dict.youdao.com/w/eng/%s/#keyfrom=dict2.index" % word
+
+	html = requests.get(url)
+
+	doc = etree.HTML(html.text)
+
+	s = pattern_1(doc)
+
+	if s == "":
+		s = pattern_2(doc)
+	if s != "":
+		save_content(word, s)
+		download_audio(word)
+	return s
+
+def load_from_cache(word):
+	file_dir = "%s/%s/" % (data_dir, word)
+	if not os.path.exists(file_dir):
+		return ""
+	file_name = "%s/content.txt" % file_dir
+	if not os.path.exists(file_name):
+		return ""
+	file = open(file_name, 'r')
+	s = file.read()
+	file.close()
+	return s
+
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+
+if len(sys.argv) != 2:
+	print "please use python search.py <word>"
+	exit(1)
+
+word = sys.argv[1]
+
+s = load_from_cache(word)
 
 if s == "":
-	s = pattern_2(doc)
+	s = load_from_net(word)
 
 if s == "":
-	print "error"
+	print "%s not found" % word
 	exit(1)
 
 print s
 
-file = download_audio(word)
-os.system("afplay %s" % file)
-
-
-
-
+audio_file = "%s/%s/audio.mp3" % (data_dir, word)
+os.system("afplay %s" % audio_file)
